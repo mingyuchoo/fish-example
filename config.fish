d8d98cde25ca33d1b021c62559962a5ecd8a821c
mingyuchoo <mingyuchoo@gmail.com>
2021-06-17 16:40:43 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -8,19 +8,21 @@ set -gx MANPATH     /usr/local/man $MANPATH
 set -gx LANG        en_US.UTF-8
 
 
-set -x  JAVA_HOME    "/Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home"
-set -x  VSCODE_PATH  "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-set -x  COURSIER     "$HOME/Library/Application Support/Coursier/bin"
-set -x  CARGO_PATH   "$HOME/.cargo"
-set -x  GHCUP_PATH   "$HOME/.ghcup"
-set -x  NODE_PATH    "$HOME/.nvm/versions/node/v12.22.1"
-set -x  NVM_DIR      "$HOME/.nvm"
+set -x  JAVA_HOME          "/Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home"
+set -x  VSCODE_PATH        "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+set -x  COURSIER           "$HOME/Library/Application Support/Coursier/bin"
+set -x  ANDROID_SDK_ROOT   "$HOME/Library/Android/sdk"
+set -x  CARGO_PATH         "$HOME/.cargo"
+set -x  GHCUP_PATH         "$HOME/.ghcup"
+set -x  NODE_PATH          "$HOME/.nvm/versions/node/v12.22.1"
+set -x  NVM_DIR            "$HOME/.nvm"
 
-# Set global environment
+
 set -gx PATH        $JAVA_HOME/bin $VSCODE_PATH $COURSIER $HOME/bin /usr/local/bin /usr/local/sbin $PATH
+set -gx PATH        $ANDROID_SDK_ROOT/emulator $ANDROID_SDK_ROOT/tools $ANDROID_SDK_ROOT/platform-tools $PATH
 set -gx PATH        $NVM_DIR $NODE_PATH/bin $GHCUP_PATH/bin $CARGO_PATH/bin $PATH
 
-# For python venv
+
 set -e _OLD_FISH_PROMPT_OVERRIDE
 set -e _OLD_VIRTUAL_PYTHONHOME
 set -e _OLD_VIRTUAL_PATH
