a7f3b8d9be635048812a930733cf59bb33ddadde
mingyuchoo <mingyuchoo@gmail.com>
2024-05-14 21:43:06 -0400

    updated

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 umask 022
 
-# ENV
+# ENVs
 set -gx EDITOR 'nvim'
 set -gx LANG en_US.UTF-8
 set -gx MANPATH "/usr/local/man:$MANPATH"
@@ -9,27 +9,37 @@ set -gx PAGER 'cat'
 set -x HIST_STAMPS "yyyy-mm-dd"
 set -x fish_history_size 20000
 
-# PATH
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/sbin
+# PATHs
+fish_add_path /home/linuxbrew/.linuxbrew/bin
+fish_add_path /home/linuxbrew/.linuxbrew/sbin
 fish_add_path /usr/local/bin
 fish_add_path $HOME/bin
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.rd/bin
 fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/.config/emacs/bin
 fish_add_path $HOME/.ghcup/bin
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.config/emacs/bin
 fish_add_path $HOME/.opam/default/bin
-fish_add_path $HOME/.rd/bin
+fish_add_path $HOME/.nvm/versions/node/v22.1.0/bin
+fish_add_path $HOME/.zig
 
-# ALIAS
+# ALIASes
 alias ls="ls --color=auto"
 alias la="ls -la"
 alias ll="ls -l"
 alias lz="lazydocker"
 alias ed="emacs --daemon"
 alias ec="emacsclient -c"
+alias xclip="xclip -selection clipboard"
+alias xsel="xsel --clipboard --input"
 
+# ENV HOOKs
+direnv hook fish | source
+zoxide init fish | source
+
+# FUNCTIONs
 function vv
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file=$tmp
@@ -40,3 +50,5 @@ function vv
     rm -f -- $tmp
 end
 
+# EVALs
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
